f00d0cd192d38901cd10f23408060efcc7237501
Rohit-Ghoderao <rohitghoderao.github@gmail.com>
2024-12-18 12:04:33 +0530

    Add files via upload
    How to sum two numbers in a class in Python?

---
+++ How to sum two numbers in a class in Python.py
@@ -0,0 +1,11 @@
+#How to sum two numbers in a class in Python?
+class Calcualtor():
+    def add(num1,num2):
+        return num1+num2
+
+num1=5
+num2=10
+
+VarCal=Calcualtor
+result=VarCal.add(10,20)
+print(result)
