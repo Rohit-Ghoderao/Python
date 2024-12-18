559d36d37d48cf8fd0ef584d7836a0cf2bb0070b
Rohit-Ghoderao <rohitghoderao.github@gmail.com>
2024-12-18 11:53:24 +0530

    Add files via upload
    Can we add integers and floating-point numbers together in Python?

---
+++ Can we add integers and floating point numbers together in Python.py
@@ -0,0 +1,12 @@
+#Can we add integers and floating-point numbers together in Python? 
+num1 = 10
+num2 = 20.66
+
+add = int(num1) + float(num2)
+sub = int(num1) - float(num2)
+mul = int(num1) * float(num2)
+div = int(num1) / float(num2)
+print(f'The additon of {num1} and {num2} is {add}')
+print(f'The sub of {num1} and {num2} is {sub}')
+print(f'The mul of {num1} and {num2} is {mul}')
+print(f'The div of {num1} and {num2} is {div}')
